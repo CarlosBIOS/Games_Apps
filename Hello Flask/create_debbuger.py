8821ea8adafa48341a74e6d91c9d5ac9a011f853
Carlos Monteiro <cmmonteiro40@gmail.com>
2024-07-18 10:11:02 +0100

    Initial commit

--- Hello Flask/create_debbuger.py
+++ Hello Flask/create_debbuger.py
@@ -4,12 +4,12 @@ import time
 
 
 def speed_calc_decorator(function):
-    def wrapper_funtion():
+    def wrapper_function():
         start_time = time.time()
         function()
         end_time = time.time()
         print(f'{function.__name__} run speed: {end_time - start_time}')
-    return wrapper_funtion
+    return wrapper_function
 
 
 @speed_calc_decorator
